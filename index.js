7eee71143a83b6b1bae3c16386c44fa30b8c39ab
Jannick Fahlbusch <git@jf-projects.de>
2015-10-04 16:24:56 +0200

    Send 200, when callback is called

--- index.js
+++ index.js
@@ -10,34 +10,46 @@ var dioder = require('dioder')({
     colorComponentPins: [18, 17, 4]
 });
 
+var animationDuration = 1500
+	easing = 'easeInOutQuad';
+
 app.get('/api/color/:color', function(req, res) {
 	switch (req.params.color) {
 		case 'red':
-			dioder.animateTo('#f00');
+			dioder.animateTo('#f00', animationDuration, easing, function() {
+				res.json({'Color': 'red'});
+			});
 			break;
 		case 'green':
-			dioder.animateTo('#0f0');
-			break;
+			dioder.animateTo('#0f0', animationDuration, easing, function() {
+				res.json({'Color': 'green'});
+			});			break;
 		case 'blue':
-			dioder.animateTo('#00f');
-			break;
+			dioder.animateTo('#00f', animationDuration, easing, function() {
+				res.json({'Color': 'blue'});
+			});			break;
 	}
-	res.json({'Color': req.params.color});
 });
 
 app.get('/api/hex/:hexCode', function(req, res) {
-	dioder.animateTo(req.params.hexCode);
+	dioder.animateTo(req.params.hexCode, animationDuration, easing, function() {
+		res.json({'ColorHex': req.params.hexCode});
+	});
 });
 
 app.get('/api/rgb/:red/:green/:blue', function(req, res) {
 	var color = [req.params.red, req.params.green, req.params.blue];
-	dioder.animateTo(color);
+	dioder.animateTo(color, animationDuration, easing, function() {
+		res.json({'Color': color})
+	});
 });
 
 app.get('/api/off', function(req, res) {
 	//See https://github.com/ffraenz/dioder/pull/1
 	//dioder.turnOff();
-	dioder.animateTo('#000');
+	dioder.animateTo('#000', animationDuration, easing, function() {
+		res.json({'Message': 'Turned off'});
+	});
 });
 
 app.listen(PORT);
